89d75471d0c292bb7ab05ef172cbfb2515d41f05
João Vittor Cesar <163493076+joaovittorcesar@users.noreply.github.com>
2024-03-21 21:12:16 -0300

    Add files via upload

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Feito pelo joaovittorcesar - O Brabo!");
+            Console.WriteLine("https://github.com/joaovittorcesar");
+            Console.Write($"{Environment.NewLine}Gerador de números aleatórios{Environment.NewLine}{Environment.NewLine}");
+
+            Random rnd = new Random();
+
+            int numeroSinCotaArbitraria = rnd.Next();
+            Console.WriteLine(">> Número sem cotas: {0}", numeroSinCotaArbitraria);
+
+            int numeroConCotaSuperior = rnd.Next(0, 6);
+            Console.WriteLine(">> Número entre 0 e 5: {0}", numeroConCotaSuperior);
+
+            int numeroConDosCotas = rnd.Next(4, 10);
+            Console.WriteLine(">> Número entre 4 e 9: {0}", numeroConDosCotas);
+
+            Console.Write($"{Environment.NewLine}Pressione qualquer tecla para sair...{Environment.NewLine}");
+Console.ReadKey(true);
+        }
+    }
+}
